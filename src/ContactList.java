493120deafc3a379c973a70bd5eba4e9fd769c37
teddymeeks <teddy.meek@menloschool.org>
2024-01-15 23:23:58 -0800

    Done all three of Person, Student, and Athelete, pretty far through ContactList, just haven't dont sort and onwards

---
+++ src/ContactList.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class ContactList
+{
+    private ArrayList<Person> contacts;
+
+    public ContactList()
+    {
+        contacts = new ArrayList<Person>();
+    }
+
+    //check back because you need to return it as the correct thing i.e Student vs athelete
+    public String getContacts(Person s)
+    {
+        if (s instanceof Student)
+        {
+            String gradeAsString = Integer.toString(((Student) s).getGrade());
+            return gradeAsString;
+        }
+        if (s instanceof Athlete)
+        {
+            return ((Athlete) s).getSport();
+        }
+        //ask about what it's supposed to return if not one of your things
+        return null;
+    }
+
+    //might be wiggity, check back
+    public void addContacts(Person newContact)
+    {
+        contacts.add(newContact);
+    }
+
+    public void printContacts()
+    {
+        for (Person s : contacts)
+        {
+            if (s instanceof Student)
+            {
+                ((Student) s).toString();
+            }
+            if (s instanceof Athlete)
+            {
+                ((Athlete) s).toString();
+            }
+        }
+    }
+
+    public void sort()
+    {
+
+    }
+}
