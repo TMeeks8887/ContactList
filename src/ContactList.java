db9fe884035c52dad3db915144eb02e7e5fda469
teddymeeks <teddy.meek@menloschool.org>
2024-01-16 14:59:27 -0800

    figured out getContacts

--- src/ContactList.java
+++ src/ContactList.java
@@ -12,17 +12,17 @@ public class ContactList
     //check back because you need to return it as the correct thing i.e Student vs athelete
     public String getContacts(Person s)
     {
-        if (s instanceof Student)
-        {
-            String gradeAsString = Integer.toString(((Student) s).getGrade());
-            return gradeAsString;
-        }
-        if (s instanceof Athlete)
-        {
-            return ((Athlete) s).getSport();
-        }
+//        if (s instanceof Student)
+//        {
+//            String gradeAsString = Integer.toString(((Student) s).getGrade());
+//            return gradeAsString;
+//        }
+//        if (s instanceof Athlete)
+//        {
+//            return ((Athlete) s).getSport();
+//        }
         //ask about what it's supposed to return if not one of your things
-        return null;
+        return contacts;
     }
 
     //might be wiggity, check back
@@ -35,14 +35,7 @@ public class ContactList
     {
         for (Person s : contacts)
         {
-            if (s instanceof Student)
-            {
-                ((Student) s).toString();
-            }
-            if (s instanceof Athlete)
-            {
-                ((Athlete) s).toString();
-            }
+            s.toString();
         }
     }
 
